c2739b848001fef1f24601e9af5cee712a645ffb
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-09-10 13:10:22 +0100

    error page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
-export default async function MealsPage(){
-const meals=await getMeals();
+async function Meals(){
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
+export default  function MealsPage(){
   return <>
   <header className={classes.header}>
     <h1>
@@ -19,7 +23,9 @@ const meals=await getMeals();
   </header>
   
   <main className={classes.main}>
-    <MealsGrid meals={meals} />
+<Suspense fallback={<p className={classes.loading}>Fetching meals ...</p>}>
+  <Meals />
+  </Suspense>
   </main>
   
   </>;
